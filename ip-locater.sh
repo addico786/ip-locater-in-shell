7d8b38ce87c08d742c08515b7e2ba07180c7b0fb
Adnan Khan <adnandelhi2004@gmail.com>
2025-04-24 12:12:50 +0300

    Create ip-locater.sh

---
+++ ip-locater.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]; then
+    echo "usage: $0 IP"
+    echo "please provide an ip "
+    exit
+fi
+
+res=$(curl http://ip-api.com/json/$1 -s)
+
+status=$(echo $res | jq '.status' -r)
+
+if [ $status = "success" ]
+then
+        city=$(echo $res | jq '.city' -r)
+        echo "CITY:$city"
+        region=$(echo $res | jq '.regionName' -r)
+        echo "REGION: $region"
+        country=$(echo $res | jq '.country' -r)
+        echo "COUNTRY: $country"
+
+
+fi
